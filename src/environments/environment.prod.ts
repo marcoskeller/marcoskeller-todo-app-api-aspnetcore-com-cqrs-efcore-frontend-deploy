d8234acbd29d14f7e301fd4c0f1ec86ecffac608
marcos.fonseca <marcos.fonseca@squadra.com.br>
2022-02-05 15:13:17 -0300

    Finalizado o frontEnd da Api Lista de Tarefas

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: true,
+  firebase: {
+    // apiKey: "AIzaSyDCGPTiXhgDwQIyOp5iiQv7KvGsN2EIusI",
+    // authDomain: "todos-daba8.firebaseapp.com",
+    // databaseURL: "https://todos-daba8.firebaseio.com",
+    // projectId: "todos-daba8",
+    // storageBucket: "todos-daba8.appspot.com",
+    // messagingSenderId: "381634863399",
+    // appId: "1:381634863399:web:6811d0527a323c5a368902",
+    // measurementId: "G-J2CV32GTSY"
+
+    //Token Gerado no FireBase do Google
+    apiKey: "AIzaSyAmC0RVxaKKqw3OlnKqzTLpD7Ch_jwd1WU",
+    authDomain: "todos-8f6a4.firebaseapp.com",
+    projectId: "todos-8f6a4",
+    storageBucket: "todos-8f6a4.appspot.com",
+    messagingSenderId: "399368697130",
+    appId: "1:399368697130:web:9bd951806a82b62f1ada53"
+
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,35 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    // apiKey: "AIzaSyDCGPTiXhgDwQIyOp5iiQv7KvGsN2EIusI",
+    // authDomain: "todos-daba8.firebaseapp.com",
+    // databaseURL: "https://todos-daba8.firebaseio.com",
+    // projectId: "todos-daba8",
+    // storageBucket: "todos-daba8.appspot.com",
+    // messagingSenderId: "381634863399",
+    // appId: "1:381634863399:web:6811d0527a323c5a368902",
+    // measurementId: "G-J2CV32GTSY"
+
+    //Token Gerado no FireBase do Google
+    apiKey: "AIzaSyAmC0RVxaKKqw3OlnKqzTLpD7Ch_jwd1WU",
+    authDomain: "todos-8f6a4.firebaseapp.com",
+    projectId: "todos-8f6a4",
+    storageBucket: "todos-8f6a4.appspot.com",
+    messagingSenderId: "399368697130",
+    appId: "1:399368697130:web:9bd951806a82b62f1ada53"
+    
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
